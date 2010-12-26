89f9ac6b08bd0c3ef20e0a74ed48d00aa46197a3
Michael Yanovich <yanovich.1@osu.edu>
2010-12-26 07:51:31 -0500

    moved function outside of while loop for efficiency, and moved a few other things around as well

--- birthday.py
+++ birthday.py
@@ -1,48 +1,54 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
-print "This will simulate the 'Birthday Problem' by generating XX random numbers between 1 and 365 (inclusively).\n"
+import math, random
+from decimal import Decimal
+
+print "This will simulate the 'Birthday Problem' by generating XX random"
+print "numbers between 1 and 365 (inclusively).\n"
+
 people = int(raw_input("Please enter how many random 'people' you'd like to simulate: "))
 
 trials = int(raw_input("Please enter how many trials you'd like to complete: "))
-
-import random, math, decimal
 
 matches = 0
 nomatches = 0
 total_trials = trials
 
+def findmatches( numbers ):
+    s = set()
+    ans = False
+    for number in numbers:
+        if numbers.count(number) > 1:
+            s.add(number)
+            ans = True
+    return ans, s
+
 while trials > 0:
     trials -= 1
+    
     # Seed random with the current time, creates more "randomness" on each iteration
     random.seed()
     
+    #generate a list of "people" length, where each element is a random number between from 1 (inclusive) to 366 (non-inclusive)
     numbers = [random.randrange(1,366) for i in range(people)]
 
     #print "Here is the list of numbers: "
-
     #for number in numbers:
         #print number
         
-    def findmatches( numbers ):
-        s = set()
-        ans = False
-        for number in numbers:
-            if numbers.count(number) > 1:
-                s.add(number)
-                ans = True
-        return ans, s
-
     a,b = findmatches(numbers)
     
     if a == True:
         matches += 1
+    
+    #print "Trial:",total_trials - trials
 
-result = float (matches) / total_trials * 100
-expected =  (1 - decimal.Decimal(math.factorial(365)) / ( 365**people * math.factorial(365-people)))*100
-pe = abs( (expected - decimal.Decimal(result) ) / decimal.Decimal(result)) * 100
+result = Decimal(matches) / Decimal(total_trials) * 100
+expected =  (1 - Decimal(math.factorial(365)) / ( 365**people * math.factorial(365-people)))*100
+pe = abs( (expected - Decimal(result) ) / Decimal(result)) * 100
 
-print "\nNumber of successful trials: ", matches
-print "Percent of successful trials: ",result
-print "Expected percent of number of successful trials:",expected
-print "Percent of error:",pe
+print "\n\nNumber of successful trials: ", matches
+print "Percent of successful trials: ",round(result,4)
+print "Expected percent of number of successful trials:",round(expected,4)
+print "\nPercent of error:",round(pe,4)
 
